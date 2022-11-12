f520b912982a3a924b4dd92339ef500934fc039a
Roberto Guevara <robertoeguevara@gmail.com>
2022-11-12 14:46:52 -0800

    fix: api calls

--- google/index.js
+++ google/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, UI_URI, SERVER_URI } from './config';
+import cors from 'cors';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, UI_URI, SERVER_URI } from './config.js';
 import axios from 'axios';
 
 const port = 8181;
 const app = express();
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
@@ -11,8 +13,23 @@ app.listen(port, () => {
 
 app.get('/', async(req, res) => {
     const code = req.query.code;
-
-    const token = await axios.post(`https://oauth2.googleapis.com/token?code=${code}&client_id=${GOOGLE_CLIENT_ID}&client_secret=${GOOGLE_CLIENT_SECRET}&redirect_uri=${UI_URI}/auth&grant_type=authorization`);
-    const userInfo = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${token.data.access_token}`);
-    return res.send(userInfo.data);
+    console.log(code);
+    if (code) {
+      //console.log('code is present');
+        const response = await axios.post('https://oauth2.googleapis.com/token', {
+            client_id: GOOGLE_CLIENT_ID,
+            client_secret: GOOGLE_CLIENT_SECRET,
+            code: code,
+            redirect_uri: UI_URI,
+            grant_type: 'authorization_code'
+        });
+        //const userInfo = await axios.get(`https://www.googleapis.com/oauth2/v2/userinfo?access_token=${response.data.access_token}`);
+        const userInfo = await axios.get('https://www.googleapis.com/oauth2/v2/userinfo?alt=json', {
+            headers: {
+              authorization: `Bearer ${response.data.access_token}`
+            }
+        });
+        res.send(userInfo.data);
+    }
+    else { res.send('No code'); }
 });
